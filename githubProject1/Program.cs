0c2817fb21374d1eb6120a90acbf3396736cc019
Mohamed3bdou <116263042+Mohamed3bdou@users.noreply.github.com>
2022-10-23 16:02:58 +0200

    First Upload

---
+++ githubProject1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace githubProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
